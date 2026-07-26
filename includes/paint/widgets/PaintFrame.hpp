07f38ef190970e6df8a29550982d76b9b75bf6dc
pcapurro <fossa634@gmail.com>
2026-07-26 12:08:35 +0200

    PaintFrame.hpp indentations update

--- includes/paint/widgets/PaintFrame.hpp
+++ includes/paint/widgets/PaintFrame.hpp
@@ -55,54 +55,54 @@ class Action
 class PaintFrame : public Element
 {
     private:
-        const int               _displayMode;
+        const int                       _displayMode;
 
-        vector<Shape>           _pngBack;
-        optional<Texture>       _paintTexture;
+        vector<Shape>                   _pngBack;
+        optional<Texture>               _paintTexture;
 
-        vector<vector<Color>>   _paintData;
+        vector<vector<Color>>           _paintData;
 
-        Color                   _pickedColor;
+        Color                           _pickedColor;
 
-        int                     _selectedTool;
-        int                     _brushSize;
-        Color                   _selectedColor;
+        int                             _selectedTool;
+        int                             _brushSize;
+        Color                           _selectedColor;
 
         bool                            _timeLineSwitch;
         int                             _timeLineCursor;
         vector<std::optional<Action>>   _timeLine;
 
-        std::pair<int, int>     _lineStart = { -1, -1 };
-        std::pair<int, int>     _lineEnd = { -1, -1 };
+        std::pair<int, int>             _lineStart = { -1, -1 };
+        std::pair<int, int>             _lineEnd = { -1, -1 };
 
-        std::pair<int, int>     _rectStart = { -1, -1 };
-        std::pair<int, int>     _rectEnd = { -1, -1 };
+        std::pair<int, int>             _rectStart = { -1, -1 };
+        std::pair<int, int>             _rectEnd = { -1, -1 };
 
-		static constexpr int	PngWidth = 30;
-		static constexpr int	PngHeight = 30;
+		static constexpr int	        PngWidth = 30;
+		static constexpr int	        PngHeight = 30;
 
-        void                    initPngBack(void);
+        void                            initPngBack(void);
 
-        void                    initPaintData(void);
-        void                    initPaintTexture(SDL_Renderer* renderer);
+        void                            initPaintData(void);
+        void                            initPaintTexture(SDL_Renderer* renderer);
 
-        void                    addAction(const int code, const Parameter& parameter);
+        void                            addAction(const int code, const Parameter& parameter);
 
-        void                    paintBrush(const int x, const int y);
-        void                    paintPencil(const int x, const int y);
+        void                            paintBrush(const int x, const int y);
+        void                            paintPencil(const int x, const int y);
 
-        void                    paintBucket(const int x, const int y);
-        void                    paintSpray(const int x, const int y);
+        void                            paintBucket(const int x, const int y);
+        void                            paintSpray(const int x, const int y);
 
-        void                    erase(const int x, const int y);
-        void                    pick(const int x, const int y);
+        void                            erase(const int x, const int y);
+        void                            pick(const int x, const int y);
 
-        void                    paintRectangle(const int startX, const int startY, \
-                                    const int endX, const int endY);
-        void                    paintLine(const int startX, const int startY, \
-                                    const int endX, const int endY);
+        void                            paintRectangle(const int startX, const int startY, \
+                                            const int endX, const int endY);
+        void                            paintLine(const int startX, const int startY, \
+                                            const int endX, const int endY);
 
-        void                    updateTexture(void);
+        void                            updateTexture(void);
 
     public:
         PaintFrame(const int x, const int y, const int width, \
@@ -111,31 +111,31 @@ class PaintFrame : public Element
 
         ~PaintFrame(void) = default;
 
-        void                    render(SDL_Renderer* renderer);
+        void                            render(SDL_Renderer* renderer);
 
-        void                    clearTimeLine(void);
-        void                    clearPainting(void);
+        void                            clearTimeLine(void);
+        void                            clearPainting(void);
 
-        Color                   getPickedColor(void) const noexcept;
+        Color                           getPickedColor(void) const noexcept;
 
-        void                    execAction(const int code, const Parameter& parameter);
-        void                    replay(void);
-        void                    back(void);
-        void                    forward(void);
+        void                            execAction(const int code, const Parameter& parameter);
+        void                            replay(void);
+        void                            back(void);
+        void                            forward(void);
 
-        void                    setSelectedTool(const int tool);
-        void                    setBrushSize(const int newBrushSize);
-        void                    setSelectedColor(const Color& newColor);
+        void                            setSelectedTool(const int tool);
+        void                            setBrushSize(const int newBrushSize);
+        void                            setSelectedColor(const Color& newColor);
 
-        virtual void            onMouseDown([[maybe_unused]] const bool held = false, \
+        virtual void                    onMouseDown([[maybe_unused]] const bool held = false, \
             [[maybe_unused]] const int x = 0, [[maybe_unused]] const int y = 0, \
             [[maybe_unused]] SDL_Renderer* renderer = nullptr) override;
-        virtual void            onMouseUp([[maybe_unused]] const int x = 0, [[maybe_unused]] const int y = 0, \
+        virtual void                    onMouseUp([[maybe_unused]] const int x = 0, [[maybe_unused]] const int y = 0, \
             [[maybe_unused]] SDL_Renderer* renderer = nullptr) override;
 
-		virtual void            onMouseHover([[maybe_unused]] const int x = 0, [[maybe_unused]] const int y = 0, \
+		virtual void                    onMouseHover([[maybe_unused]] const int x = 0, [[maybe_unused]] const int y = 0, \
             [[maybe_unused]] SDL_Renderer* renderer = nullptr) override;
-        virtual void            onMouseHoverOutside([[maybe_unused]] SDL_Renderer* renderer = nullptr) override;
+        virtual void                    onMouseHoverOutside([[maybe_unused]] SDL_Renderer* renderer = nullptr) override;
 };
 
 #endif
